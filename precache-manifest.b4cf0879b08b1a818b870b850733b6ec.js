1bab417c6c980d52c3b7bf136b1c129ff526d0c8
hoangpersie <hoangpersievn@gmail.com>
2019-12-15 09:06:16 +0700

    Updates

---
+++ precache-manifest.b4cf0879b08b1a818b870b850733b6ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6583963480b0046d1888d23b760bb0f5",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d9f738e028b63f974d23",
+    "url": "/todolist/static/css/main.d37513cd.chunk.css"
+  },
+  {
+    "revision": "54cab9744683502254c1",
+    "url": "/todolist/static/js/2.47c913cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.47c913cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9f738e028b63f974d23",
+    "url": "/todolist/static/js/main.8c448967.chunk.js"
+  },
+  {
+    "revision": "b5631cdcdd14849564a7",
+    "url": "/todolist/static/js/runtime-main.75b5d516.js"
+  },
+  {
+    "revision": "d29f1779664178a55ed87236fd97c302",
+    "url": "/todolist/static/media/check.d29f1779.svg"
+  },
+  {
+    "revision": "2f236e6fb2303cc94fdacdff7a82957c",
+    "url": "/todolist/static/media/down_button.2f236e6f.svg"
+  },
+  {
+    "revision": "40eb9cc6d51d63d1dfd8a0ea29b8d700",
+    "url": "/todolist/static/media/uncheck.40eb9cc6.svg"
+  }
+]);
